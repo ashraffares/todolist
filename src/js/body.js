4cc275800c8f66075f4a8ad949971cbbffd5aeb5
Fares <ashraffares090@gmail.com>
2021-06-04 12:01:58 +0200

    create new project form

--- src/js/body.js
+++ src/js/body.js
@@ -1,5 +1,53 @@
 import '../css/style.css';
 
+function createProjectForm() {
+  const overlayp = document.createElement('div');
+  overlayp.classList.add('overlayp');
+
+  const form = document.createElement('form');
+  form.classList.add('form');
+
+  const formh = document.createElement('div');
+  formh.classList.add('form-h');
+
+  const formtitle = document.createElement('h2');
+  formtitle.classList.add('form-title');
+  formtitle.textContent = 'New Task';
+
+  const formclose = document.createElement('button');
+  formclose.classList.add('form-close');
+  formclose.textContent = 'X';
+
+  formh.appendChild(formtitle);
+  formh.appendChild(formclose);
+
+  const hr = document.createElement('hr');
+
+  const label = document.createElement('label');
+  label.classList.add('label');
+  label.textContent = 'Name';
+  label.htmlFor = 'project';
+
+  const inputp = document.createElement('input');
+  inputp.classList.add('inputs');
+  inputp.id = 'project';
+  inputp.type = 'text';
+
+  const buttonp = document.createElement('button');
+  buttonp.classList.add('button');
+  buttonp.textContent = 'Create Project';
+
+  form.appendChild(formh);
+  form.appendChild(hr);
+  form.appendChild(label);
+  form.appendChild(inputp);
+  form.appendChild(buttonp);
+
+  overlayp.appendChild(form);
+  document.querySelector('#parent').appendChild(overlayp);
+  return overlayp;
+}
+
 export default function main() {
   const nav = document.createElement('div');
   nav.classList.add('nav');
@@ -39,8 +87,9 @@ export default function main() {
   const parent = document.getElementById('parent');
   parent.appendChild(nav);
   parent.appendChild(main);
-}
 
-function forms() {
-
+  const projectForm = createProjectForm();
+  sideleftli.onclick = () => {
+    projectForm.style.display = 'block';
+  };
 }
