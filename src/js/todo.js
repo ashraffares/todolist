205732d3cf8b147ec9568440b139e08cd82ccd74
chasscepts <francischassobetta@gmail.com>
2021-06-10 13:09:48 +0100

    Refactor to allow merge

---
+++ src/js/todo.js
@@ -0,0 +1,7 @@
+export default function Todo(title, description, duedate, priority, isDone) {
+  this.title = title;
+  this.description = description;
+  this.duedate = duedate;
+  this.priority = priority;
+  this.isDone = isDone;
+}
